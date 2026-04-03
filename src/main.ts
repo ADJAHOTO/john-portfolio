0de5732de31fa5db9dedccb6d6cc8a667c389bb3
adjahoto <benisseadjahoto@gmail.com>
2026-04-03 11:43:25 +0100

    surlignage dans le navbar done

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import router from './router'
 
 import App from './App.vue'
 
+// Permet de revenir au debut de la page lors de la navigation
+if ('scrollRestoration' in history) {
+  history.scrollRestoration = 'manual'
+}
+window.scrollTo(0, 0)
+
 const app = createApp(App)
 
 app.use(createPinia())
